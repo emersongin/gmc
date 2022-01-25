f01fe0b2fbb20fce089d4569e89e88ef7cca0e9e
= <=>
2022-01-24 22:29:07 -0300

    npm start pg

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const express = require('express');
 const cors = require('cors');
+
+const { Client } = require('pg');
+const client = new Client({
+    user:     'postgres',
+    host:     'localhost',
+    database: 'guia',
+    password: '1234',
+    port:     5433
+});
+
 const server = express();
 
 const host = '10.0.0.109';
@@ -9,8 +19,12 @@ server.use(express.urlencoded({ extended: true }));
 server.use(express.json());
 server.use(cors());
 
-server.get('/api', function(req, res, next) {
-    res.json({"msg": "teste"});
+server.get('/api', async function(req, res, next) {
+    await client.connect();
+    const response = await client.query('SELECT * FROM events');
+    await client.end();
+
+    res.json({"msg": response.rows[0]});
 });
 
 server.listen(port, host, () => {
