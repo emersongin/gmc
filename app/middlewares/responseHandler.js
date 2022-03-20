08fd59be60a5f40790733a754fda6f4bab3e1de8
emersongin <emersonandrey_7@hotmail.com>
2022-03-20 20:02:32 -0300

    responseHandler

--- app/middlewares/responseHandler.js
+++ app/middlewares/responseHandler.js
@@ -1,27 +1,32 @@
 module.exports = (req, res, next) => {
-
-    const success = data => {
-        res.status(200);
-
-        return {
-            result: true,
-            status: 200,
-            data
-        };
+    const respond = params => {
+        res.status(params.status);
+        
+        return res.json({
+            result: params.result,
+            status: params.status,
+            data: params.data
+        });
     };
 
-    const error = error => {
-        res.status(400);
-
-        return {
+    const success = (data, codeStatus) => {
+        return respond({
+            result: true,
+            status: codeStatus || 200,
+            data: data || []
+        });
+    };
+    
+    const error = (error, codeStatus) => {
+        return respond({
             result: false,
-            status: 400,
-            data: error
-        };
+            status: codeStatus || 400,
+            data: error || []
+        });
     };
 
     res.success = success;
     res.error = error;
 
     next();
-}
+};
